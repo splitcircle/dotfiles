78943ccb15363144f2eea16f82601aac8d37afe4
Chris Callan <chriscallanjr@me.com>
2021-08-12 11:09:34 -0400

    updates

--- fish/config.fish
+++ fish/config.fish
@@ -5,5 +5,11 @@ alias pfetch='sh ~/dev/etc/pfetch/pfetch'
 alias moon='curl wttr.in/moon'
 alias weather='curl wttr.in/philadelphia'
 alias work='cd ~/dev/wwd/be'
+alias angora='$HOME/.virtualenv/angora/bin/angora'
+set -g fish_user_paths "/usr/local/Cellar/ruby/3.0.0_1/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/lib/ruby/gems/3.0.0/bin" $fish_user_paths
+set -x WBDL_HOME "$HOME/dev/wbdl"
+set -x DFHOME "$HOME/dev/wbdl"
+status --is-interactive; and source (rbenv init -|psub)
 
 starship init fish | source
